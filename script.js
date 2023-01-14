38c87d6b7f876962b3e47e4c0945cbef96f3a247
ankitdebnath003 <122445989+ankitdebnath003@users.noreply.github.com>
2023-01-14 14:50:10 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+$(document).ready(function () {
+    $(".firstmenu").click(function () {
+        $(".navmenu").toggleClass('firstmenu');;
+    });
+});
+
+
+$(document).ready(function () {
+    $(".secondmenu").click(function () {
+        $(".navbar").toggleClass('secondmenu');;
+    });
+});
